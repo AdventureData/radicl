ff3172f1dd062f58dde8855333336c4aaf947b2d
micah johnson <micah.johnson150@gmail.com>
2019-01-19 09:37:16 -0700

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/adventuredata/radicl',
-    version='0.2.0',
+    version='0.2.1',
     zip_safe=False,
     scripts = ['scripts/plotlyte',],
      entry_points = {
