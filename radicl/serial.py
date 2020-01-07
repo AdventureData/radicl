6e21ffd5541a07d25ceb6b48401cb0d1c8ed10b0
Adrian Rothenbuhler <arothenbuhler@bastiansolutions.com>
2020-01-06 19:23:14 -0700

    Small fix in serial driver to always invalidate port when closing it

--- radicl/serial.py
+++ radicl/serial.py
@@ -83,6 +83,7 @@ class RAD_Serial():
 	def closePort(self):
 		if (self.serial_port != None):
 			self.serial_port.close
+			self.serial_port = None
 
 	def flushPort(self):
 		if (self.serial_port != None):
