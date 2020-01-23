ecf4bbe8e53b1a7a4601fef76c4aab65334a4497
Micah Johnson <micah.johnson150@gmail.com>
2020-01-22 17:39:29 -0700

    Updated the docs
    Bump version: 0.2.5 → 0.3.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/adventuredata/radicl',
-    version='0.2.5',
+    version='0.3.0',
     zip_safe=False,
     scripts = ['scripts/plotlyte',],
      entry_points = {
