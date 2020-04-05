88bad4fe76f4301ee13cd3f4654a3eb8c4164145
Micah Johnson <micah.johnson150@gmail.com>
2020-04-04 22:27:23 -0600

    Added in tests

--- tests/test_probe_data.py
+++ tests/test_probe_data.py
@@ -5,20 +5,58 @@ import matplotlib.pyplot as plt
 
 from radicl.probe import RAD_Probe
 
-
 class TestProbeData(unittest.TestCase):
+    '''
+    Test all the data with lower sample rates
+    '''
+    measure_time = 0.5
 
     @classmethod
     def setUpClass(self):
-        self.probe = RAD_Probe()
+        self.probe = RAD_Probe(debug=True)
         self.probe.resetMeasurement()
         self.probe.startMeasurement()
-        time.sleep(0.1)
+        time.sleep(self.measure_time)
         self.probe.stopMeasurement()
 
     @classmethod
     def tearDownClass(self):
         self.probe.resetMeasurement()
+
+class TestSmallProbeData(TestProbeData):
+    '''
+    Test all the data with lower sample rates so we take a larger measurement
+    '''
+    measure_time = 0.5
+
+    def test_get_FilteredDepth_data(self):
+        """
+        Test retrieve filtered depth data from the probe
+        """
+        try:
+            d = self.probe.readFilteredDepthData()
+            assert d is not None
+
+        except Exception as e:
+            raise(e)
+
+
+    def test_get_raw_accleration_depth_data(self):
+        """
+        Test retrieve acceleration data from the probe
+        """
+        try:
+            d = self.probe.readRawAccelerationData()
+            assert d is not None
+
+        except Exception as e:
+            raise(e)
+
+class TestLargeProbeData(TestProbeData):
+    '''
+    Test all the data with lower sample rates
+    '''
+    measure_time = 0.1
 
     def test_get_calibrated_data(self):
         """
@@ -30,42 +68,6 @@ class TestProbeData(unittest.TestCase):
 
         except Exception as e:
             raise(e)
-    
-    def test_get_FilteredDepth_data(self):
-        """
-        Test retrieve filtered depth data from the probe
-        """
-        try:
-            d = self.probe.readFilteredDepthData()
-            print(d)
-            assert d is not None
-
-        except Exception as e:
-            raise(e)
-
-
-    # def test_get_raw_accleration_depth_data(self):
-    #     """
-    #     Test retrieve acceleration data from the probe
-    #     """
-    #     try:
-    #         d = self.probe.readRawAccelerationData()
-    #         print(d)
-    #         assert d is not None
-    #
-    #     except Exception as e:
-    #         raise(e)
-    #
-    # def test_get_sensor_combo_depth_data(self):
-    #     """
-    #     Test retrieve APp version of the data from the probe
-    #     """
-    #     try:
-    #         d = self.probe.readSensorDepthComboData()
-    #         assert d is not None
-    #
-    #     except Exception as e:
-    #         raise(e)
 
 
 if __name__ == '__main__':
