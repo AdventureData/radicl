d25e7a4280b8bc5ddc408c1bed1d5ae58a39b21e
Micah Johnson <micah.johnson150@gmail.com>
2020-08-08 13:34:47 -0600

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/adventuredata/radicl',
-    version='0.4.1',
+    version='0.4.2',
     zip_safe=False,
      entry_points = {
           'console_scripts': [
