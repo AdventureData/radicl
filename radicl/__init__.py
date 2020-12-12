6326b6305834dc6a2035aeac32d2e096ff3d5e9f
micah <micah.johnson150@gmail.com>
2020-12-12 13:40:09 -0700

    Added in a backend name.
    Updated matplotlib

--- radicl/__init__.py
+++ radicl/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # coding: utf-8
 
 """Top-level package for radicl."""
@@ -5,3 +6,7 @@
 __author__ = """Micah Johnson"""
 __email__ = 'micah@adventuredata.com'
 __version__ = '0.4.2'
+
+import matplotlib
+from matplotlib import pyplot as plt
+matplotlib.use('TkAgg')
