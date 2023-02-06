ffb7359ae14201bc4e1d80cf8a5cab65a12454a2
micah johnson <micah.johnson150@gmail.com>
2023-02-06 07:08:14 -0700

    Added in new function to help with managing indicies

--- tests/test_ui_tools.py
+++ tests/test_ui_tools.py
@@ -1,6 +1,8 @@
 import inspect
 import unittest
 
+import pytest
+
 from radicl.probe import RAD_Probe
 from radicl.ui_tools import *
 
@@ -20,6 +22,14 @@ class TestUITools(unittest.TestCase):
 
         for v in ['depth', 'filtereddepth', 'rawpressure']:
             assert v in result.keys()
+
+@pytest.mark.parametrize("idx, ratio, n_samples, expected", [
+    (10, 1.1, 10, 9),
+    (1, 0.9, 10, 0)
+])
+def test_get_index_from_ratio(idx, ratio, n_samples, expected):
+    result = get_index_from_ratio(idx, ratio, n_samples)
+    assert result == expected
 
 
 if __name__ == '__main__':
