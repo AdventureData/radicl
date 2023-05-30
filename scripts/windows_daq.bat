d6f7c83ee57bdf9b676987db4b461905ed71cf30
micah johnson <micah.johnson150@gmail.com>
2023-05-30 14:54:36 -0600

    Added in windows daq script

---
+++ scripts/windows_daq.bat
@@ -0,0 +1,5 @@
+@echo off
+Running the hi res lyte probe script
+cd C:\Users\micah\OneDrive\Desktop\measurements
+call C:\Users\micah\Anaconda3\Scripts\activate.bat
+lyte_hi_res.exe --n_measurements 5
